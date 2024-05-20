4d09fc1ac099ce5fc1a0dc2b44f232c301db0086
LKM0222WIN <python03@naver.com>
2024-05-20 21:13:15 +0900

    add : ObjectPoolingManager.cs 추가 add : Slot 프리펩 추가

---
+++ Assets/01.Scripts/ObjectPoolingManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ObjectPoolingManager : MonoBehaviour
+{
+    public static ObjectPoolingManager Instance;
+
+    #region  slot
+    [SerializeField] GameObject slotPrefab;
+    [SerializeField] GameObject slotParent;
+    public Queue<GameObject> slotQueue = new Queue<GameObject>();
+    #endregion
+    private void Awake() {
+        if(Instance == null){
+            Instance = this;
+        }
+    }
+    private void Start() {
+        //slot 오브젝트 2000개 미리 생성
+        for(int i = 0; i< 2000;i++){
+            GameObject slot = Instantiate(slotPrefab, new Vector3(this.transform.position.x, this.transform.position.y, 0f), Quaternion.identity, slotParent.transform);
+            slot.SetActive(false);
+            slotQueue.Enqueue(slot);
+        }
+    }
+
+    public void InsertQueue(GameObject go, Queue queue){
+        queue.Enqueue(go);
+        go.SetActive(false);
+    }
+
+    public GameObject GetQueue(Queue<GameObject> queue){
+        GameObject go = queue.Dequeue();
+        go.SetActive(true);
+        return go;
+    }
+}
--- Assets/01.Scripts/PlayerManager.cs
+++ Assets/01.Scripts/PlayerManager.cs
@@ -8,6 +8,7 @@ public class PlayerManager : MonoBehaviour
     float xpos, ypos;
     [SerializeField] float speed;
 
+    [SerializeField] GameObject inventoryUI;
     // Update is called once per frame
     void Update()
     {
@@ -32,12 +33,28 @@ public class PlayerManager : MonoBehaviour
         if (pos.y > 1f) pos.y = 1f;
 
         this.transform.position = Camera.main.ViewportToWorldPoint(pos);
+
+        if(Input.GetKeyDown(KeyCode.I)){
+            OpenInventory();
+        }
+
     }
+
+    //인벤토리 여는 함수 추가
+    void OpenInventory(){
+        if(inventoryUI.activeSelf){
+            inventoryUI.SetActive(false);
+        }
+        else{
+            inventoryUI.SetActive(true);
+        }
+    }
+
 
     //아이템 습득
     private void OnTriggerStay2D(Collider2D other) {
         if(other.tag == "Item"){ //태그 확인
-            if(Input.GetKeyDown(KeyCode.Space)){ //아이템 습득 키 확인
+            if(Input.GetKey(KeyCode.Space)){ //아이템 습득 키 확인
                 GameManager.Instance.AddInventoryItem(other.GetComponent<ItemManager>().ReturnItem()); //인벤토리에 아이템 추가
                 print("아이템 추가됨");
                 Destroy(other.gameObject); //인벤토리에 추가해줬으니, 오브젝트를 삭제
